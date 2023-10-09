fdba2d99f51c7a6b70325c1c172e9e33f3729a5b
starcandy320 <starcandy320@outlook.com>
2023-10-10 02:48:57 +0900

    새로운 시작 첫 커밋

---
+++ app/src/main/java/com/yc/ac/gachigachi/MainActivity.java
@@ -0,0 +1,19 @@
+package com.yc.ac.gachigachi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // 메인화면은 스플래시 화면으로 사용
+
+        // 권한 허용
+
+        // 다음 액티비티로 넘기기
+    }
+}
