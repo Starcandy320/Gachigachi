89272aa96d6d739773eac736c07e93447fe7b4a8
jeong-haechan <144093354+jeong-haechan@users.noreply.github.com>
2023-11-01 17:08:10 +0900

    홈, 등교, 하교, 설정 프래그먼트 생성과 아이콘 등록 차량등록은 설정프래그먼트로 넘어가서 거기서 차량등록을 누르면 차량등록액티비티로 넘어가게 구성하는느낌으로 생각중

--- app/src/main/java/com/yc/ac/gachigachi/MainActivity.java
+++ app/src/main/java/com/yc/ac/gachigachi/MainActivity.java
@@ -1,19 +1,52 @@
 package com.yc.ac.gachigachi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // 메인화면은 스플래시 화면으로 사용
 
-        // 권한 허용
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.menu_home_fragment) {
+                    replaceFragment(new HomeFragment());
+                    return true;
+                } else if (item.getItemId() == R.id.menu_gs_fragment) {
+                    replaceFragment(new GsFragment());
+                    return true;
+                } else if (item.getItemId() == R.id.menu_gh_fragment) {
+                    replaceFragment(new GhFragment());
+                    return true;
+                } else if (item.getItemId() == R.id.menu_sys_fragment) {
+                    replaceFragment(new SystemFragment());
+                    return true;
+                }
+                return false;
+            }
+        });
 
-        // 다음 액티비티로 넘기기
+
+        replaceFragment(new HomeFragment());
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.commit();
     }
 }
