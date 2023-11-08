60f0643350d3ab5d7e9b3b500d3536084d2a1a0b
jeong-haechan <144093354+jeong-haechan@users.noreply.github.com>
2023-11-08 20:43:24 +0900

    등교리스트 구현 및 샘플데이터

--- app/src/main/java/com/yc/ac/gachigachi/GsFragment.java
+++ app/src/main/java/com/yc/ac/gachigachi/GsFragment.java
@@ -1,64 +1,44 @@
 package com.yc.ac.gachigachi;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link GsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class GsFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public GsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GsFragment newInstance(String param1, String param2) {
-        GsFragment fragment = new GsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gs, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_gs, container, false);
+
+        RecyclerView recyclerView = rootView.findViewById(R.id.recycler_view_gs);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        List<board_Item> goSchool = new ArrayList<>();
+        goSchool.add(new board_Item("등교 아이템 1", "차량번호 1", "010-0000-0000"));
+        goSchool.add(new board_Item("등교 아이템 2", "차량번호 2", "010-0000-0000"));
+        goSchool.add(new board_Item("등교 아이템 3", "차량번호 3", "010-0000-0000"));
+        goSchool.add(new board_Item("등교 아이템 4", "차량번호 4", "010-0000-0000"));
+        goSchool.add(new board_Item("등교 아이템 5", "차량번호 5", "010-0000-0000"));
+        goSchool.add(new board_Item("등교 아이템 6", "차량번호 6", "010-0000-0000"));
+        goSchool.add(new board_Item("등교 아이템 7", "차량번호 7", "010-0000-0000"));
+        goSchool.add(new board_Item("등교 아이템 8", "차량번호 8", "010-0000-0000"));
+
+        gs_ListAdapter adapter1 = new gs_ListAdapter(getActivity(), goSchool);
+
+        recyclerView.setAdapter(adapter1);
+
+        return rootView;
     }
 }
---
+++ app/src/main/java/com/yc/ac/gachigachi/board_Item.java
@@ -0,0 +1,25 @@
+package com.yc.ac.gachigachi;
+
+public class board_Item {
+    private final String title;
+    private final String subText1;
+    private final String phoneNum;
+
+    public board_Item(String title, String subText1, String phoneNum) {
+        this.title = title;
+        this.subText1 = subText1;
+        this.phoneNum = phoneNum;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSubText1() {
+        return subText1;
+    }
+
+    public String getSubText2() {
+        return phoneNum;
+    }
+}
