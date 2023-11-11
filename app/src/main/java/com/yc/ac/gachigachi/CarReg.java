dc2d75b67425d6fff6e2a825cbb10e866a3ca915
starcandy320 <starcandy320@outlook.com>
2023-11-11 11:56:38 +0900

    carAdd 부분 구현 시작

--- app/src/main/java/com/yc/ac/gachigachi/CarReg.java
+++ app/src/main/java/com/yc/ac/gachigachi/CarReg.java
@@ -1,14 +1,41 @@
 package com.yc.ac.gachigachi;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class CarReg extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_reg);
+
+        // firestore 초기화
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        // 입력한 데이터를 받아와서 가공
+        Map<String, Object> data = new HashMap<>();
+        data.put("name", "Tokyo");
+        data.put("country", "Japan");
+
+        // 자동 생성된 ID로 문서 생성
+        db.collection("car")
+                .add(data)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
     }
 }
