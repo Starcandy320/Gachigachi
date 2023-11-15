512b49c4365364a2e8ca43eb6df9b12d7eaea919
kangk <kangkw0720@naver.com>
2023-11-15 20:46:55 +0900

    gps권한추가

--- app/src/main/java/com/yc/ac/gachigachi/CarReg.java
+++ app/src/main/java/com/yc/ac/gachigachi/CarReg.java
@@ -6,13 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class CarReg extends AppCompatActivity {
-    Button Monday;
+    Button submitButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_reg);
+    Button submit = findViewById(R.id.submitButton);
 
+    submit.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(CarReg.this, "저장되었습니다.", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    });
     }
 }
