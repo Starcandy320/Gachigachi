fc48ce6c303f5a6e7a6f2d0ac1a303ca5000ac92
woota <rhaehf152415@gmail.com>
2023-11-17 14:44:26 +0900

    데이터 쓰기 예제 추가  (쓰기 실행 안됨) 쓰기 해결 후 읽기 추가예정

--- app/src/main/java/com/yc/ac/gachigachi/goToSchool.java
+++ app/src/main/java/com/yc/ac/gachigachi/goToSchool.java
@@ -1,83 +1,74 @@
 package com.yc.ac.gachigachi;
+
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.ValueEventListener;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class goToSchool extends AppCompatActivity {
 
-    private DatabaseReference mDatabase;
+    private EditText editTextName;
+    private EditText editTextCarNumber;
+    private EditText editTextAddress;
+    private EditText editPhoneNumber;
+    private Button buttonSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Firebase 인스턴스 가져오기
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        // 차량 정보 저장
-        saveCarInfo("차량ID", "차량이름", "차량모델", "차량색상");
-
-        // 차량 정보 불러오기
-        getCarInfo("차량ID");
-    }
-
-    private void saveCarInfo(String carId, String carName, String carModel, String carColor) {
-        Car car = new Car(carName, carModel, carColor);
-        mDatabase.child("cars").child(carId).setValue(car);
-    }
-
-    private void getCarInfo(String carId) {
-        mDatabase.child("cars").child(carId).addListenerForSingleValueEvent(new ValueEventListener() {
+        editTextName = findViewById(R.id.editTextName);
+        editPhoneNumber = findViewById(R.id.editPhoneNumber);
+        editTextCarNumber = findViewById(R.id.editTextCarNumber);
+        editTextAddress = findViewById(R.id.editTextAddress);
+        buttonSubmit = findViewById(R.id.buttonSubmit);
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                Car car = dataSnapshot.getValue(Car.class);
-                if (car != null) {
-                    Log.d("CarInfo", "차량 이름: " + car.getName());
-                    Log.d("CarInfo", "차량 모델: " + car.getModel());
-                    Log.d("CarInfo", "차량 색상: " + car.getColor());
-                }
+            public void onClick(View v) {
+                String name = editTextName.getText().toString();
+                String phoneNumber = editPhoneNumber.getText().toString();
+                String carNumber = editTextCarNumber.getText().toString();
+                String address = editTextAddress.getText().toString();
+
+                Map<String, Object> Car = new HashMap<>();
+                Car.put("name", name);
+                Car.put("phoneNumber", phoneNumber);
+                Car.put("carNumber", carNumber);
+                Car.put("address", address);
+
+                db.collection("CarList").document(carNumber.toString())
+                        .set(Car, SetOptions.merge())
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d(TAG, "DocumentSnapshot successfully written!");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error writing document", e);
+                            }
+                        });
             }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.w("CarInfo", "데이터 불러오기 실패", databaseError.toException());
-            }
         });
-    }
-
-    public class Car {
-        private String name;
-        private String model;
-        private String color;
-
-        public Car() {
-            // Default constructor required for calls to DataSnapshot.getValue(Car.class)
-        }
-
-        public Car(String name, String model, String color) {
-            this.name = name;
-            this.model = model;
-            this.color = color;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public String getModel() {
-            return model;
-        }
-
-        public String getColor() {
-            return color;
-        }
     }
 }
