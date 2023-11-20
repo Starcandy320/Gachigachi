b3319879dbaed832f776131aa532af6593324b5e
starcandy320 <starcandy320@outlook.com>
2023-11-20 17:30:53 +0900

    시간표 처리 완성

--- app/src/main/java/com/yc/ac/gachigachi/CarList.java
+++ app/src/main/java/com/yc/ac/gachigachi/CarList.java
@@ -1,24 +1,32 @@
 package com.yc.ac.gachigachi;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CarList {
+    //////////////// 맴버 /////////////////
     private String name;
     private String carNumber;
     private String Address;
     private String phoneNumber;
     private boolean isShow;
+    private List<Map<String, Object>> timetable;
+    /////////////// 생성자 /////////////////
     public CarList() { }
 
-    public CarList(String name, String carNumber, String address, String phoneNumber, boolean isShow) {
+    public CarList(String name, String carNumber, String address, String phoneNumber, boolean isShow, List<Map<String, Object>> timetable) {
         this.name = name;
         this.carNumber = carNumber;
         Address = address;
         this.phoneNumber = phoneNumber;
         this.isShow = isShow;
+        this.timetable = timetable;
     }
 
+    ///////////// Getter/Setter 메소드 //////////////
     public String getName() {
         return name;
     }
@@ -57,5 +65,13 @@ public class CarList {
 
     public void setShow(boolean show) {
         isShow = show;
+    }
+
+    public List<Map<String, Object>> getTimetable() {
+        return timetable;
+    }
+
+    public void setTimetable(List<Map<String, Object>> timetable) {
+        this.timetable = timetable;
     }
 }
--- app/src/main/java/com/yc/ac/gachigachi/CarReg.java
+++ app/src/main/java/com/yc/ac/gachigachi/CarReg.java
@@ -16,21 +16,32 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CarReg extends AppCompatActivity {
 
-    // editText 상단 정보
+    // 일반 정보 부분
     TextInputEditText editTextName;
     TextInputEditText editTextCarNumber;
     TextInputEditText editTextAddress;
     TextInputEditText editTextPhoneNumber;
 
-    // editText 하단 시간표
-    TextInputEditText editText;
+    // 시간표 부분
+    TextInputEditText editTextMondayArrival;
+    TextInputEditText editTextTuesdayArrival;
+    TextInputEditText editTextWednesdayArrival;
+    TextInputEditText editTextThursdayArrival;
+    TextInputEditText editTextFridayArrival;
+    TextInputEditText editTextMondayDeparture;
+    TextInputEditText editTextTuesdayDeparture;
+    TextInputEditText editTextWednesdayDeparture;
+    TextInputEditText editTextThursdayDeparture;
+    TextInputEditText editTextFridayDeparture;
 
-    // button
+    // 버튼 부분
     MaterialButton submitButton;
     MaterialButton cancelButton;
 
@@ -42,20 +53,34 @@ public class CarReg extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_reg);
 
-        // editText 윗부분
+        // 일반 정보 부분
         editTextName = findViewById(R.id.editTextName);
         editTextCarNumber = findViewById(R.id.editTextCarNumber);
         editTextAddress = findViewById(R.id.editTextAddress);
         editTextPhoneNumber = findViewById(R.id.editTextPhoneNumber);
+        
+        // 시간표 부분
+        editTextMondayArrival = findViewById(R.id.editTextMondayArrival);
+        editTextTuesdayArrival = findViewById(R.id.editTextTuesdayArrival);
+        editTextWednesdayArrival = findViewById(R.id.editTextWednesdayArrival);
+        editTextThursdayArrival = findViewById(R.id.editTextThursdayArrival);
+        editTextFridayArrival = findViewById(R.id.editTextFridayArrival);
+        editTextMondayDeparture = findViewById(R.id.editTextMondayDeparture);
+        editTextTuesdayDeparture = findViewById(R.id.editTextTuesdayDeparture);
+        editTextWednesdayDeparture = findViewById(R.id.editTextWednesdayDeparture);
+        editTextThursdayDeparture = findViewById(R.id.editTextThursdayDeparture);
+        editTextFridayDeparture = findViewById(R.id.editTextFridayDeparture);
 
         // button 부분
         submitButton = findViewById(R.id.submitButton);
         cancelButton = findViewById(R.id.cancelButton);
 
-        // firestore 초기화
+
+
+        // Firestore 초기화
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        // cancel 버튼 클릭시 동작
+        // cancelButton 클릭시 동작
         cancelButton.setOnClickListener(v -> {
             MaterialAlertDialogBuilder(context)
                     .setTitle(resources.getString(R.string.title))
@@ -73,18 +98,46 @@ public class CarReg extends AppCompatActivity {
 
 
 
-        // submit 버튼 클릭시 동작
+        // submitButton 클릭시 동작
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // 시간표 후처리
+                Map<String, Object> mon = new HashMap<>();
+                mon.put("goSchool", editTextMondayArrival);
+                mon.put("goHome", editTextMondayDeparture);
+                mon.put("isShow_day", true);
+                Map<String, Object> tue = new HashMap<>();
+                tue.put("goSchool", editTextTuesdayArrival);
+                tue.put("goHome", editTextTuesdayDeparture);
+                tue.put("isShow_day", true);
+                Map<String, Object> wed = new HashMap<>();
+                wed.put("goSchool", editTextWednesdayArrival);
+                wed.put("goHome", editTextWednesdayDeparture);
+                wed.put("isShow_day", true);
+                Map<String, Object> thu = new HashMap<>();
+                thu.put("goSchool", editTextThursdayArrival);
+                thu.put("goHome", editTextThursdayDeparture);
+                thu.put("isShow_day", true);
+                Map<String, Object> fri = new HashMap<>();
+                fri.put("goSchool", editTextFridayArrival);
+                fri.put("goHome", editTextFridayDeparture);
+                fri.put("isShow_day", true);
+                List<Map<String, Object>> timetable = new ArrayList<>();
+                timetable.add(mon);
+                timetable.add(tue);
+                timetable.add(wed);
+                timetable.add(thu);
+                timetable.add(fri);
+
                 // 입력한 데이터를 받아와서 CarList 객체에 삽입
-                // 시간표에 미입력시 공강 처리 구현 필요
                 CarList carList = new CarList(
                         editTextName.toString(),
                         editTextCarNumber.toString(),
                         editTextAddress.toString(),
                         editTextPhoneNumber.toString(),
-                        true
+                        true,
+                        timetable
                 );
 
                 // 자동 생성된 ID로 문서 생성
