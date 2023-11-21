b37694adeecd0ca0acc20e7e19d17b3dc43929f5
kangk <kangkw0720@naver.com>
2023-11-21 12:01:47 +0900

    ..

--- app/src/main/java/com/yc/ac/gachigachi/SystemFragment.java
+++ app/src/main/java/com/yc/ac/gachigachi/SystemFragment.java
@@ -1,5 +1,6 @@
 package com.yc.ac.gachigachi;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,6 +11,8 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class SystemFragment extends Fragment {
 
@@ -41,18 +44,26 @@ public class SystemFragment extends Fragment {
     }
 
     private void showDeleteConfirmationDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(requireContext());
         builder.setTitle("차량 정보 삭제")
-                .setMessage("정말로 차량정보를 삭제하시겠습니까?")
-                .setPositiveButton("예", (dialog, which) -> {
-                    // "예" 버튼이 클릭된 경우 데이터 삭제 작업 수행
-                    performDeleteOperation();
+                .setMessage("정말로 차량 정보를 삭제하시겠습니까?")
+                .setPositiveButton("예", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // "예" 버튼이 클릭된 경우 데이터 삭제 작업 수행
+                        performDeleteOperation();
+                    }
                 })
-                .setNegativeButton("아니오", null) // "아니오" 버튼 추가
+                .setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // "아니오" 버튼 클릭 시 처리할 내용 추가 가능
+                    }
+                })
                 .show();
     }
 
     private void performDeleteOperation() {
-        // 데이터 삭제 작업을 수행하는 코드를 여기에 추가
+        // 데이터 삭제 작업을 수행 하는 코드를 여기에 추가
     }
 }
