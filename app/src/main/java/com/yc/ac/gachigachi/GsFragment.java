27eac38888503cb064bcb2155466848475751e78
woota <rhaehf152415@gmail.com>
2023-11-22 21:59:16 +0900

    gsFregment 수정

--- app/src/main/java/com/yc/ac/gachigachi/GsFragment.java
+++ app/src/main/java/com/yc/ac/gachigachi/GsFragment.java
@@ -1,18 +1,33 @@
 package com.yc.ac.gachigachi;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class GsFragment extends Fragment {
+
+    private gs_ListAdapter adapter1; // gs_ListAdapter 인스턴스를 클래스 변수로 선언
+    private ArrayList<User> goSchool; // 데이터를 담을 ArrayList를 클래스 변수로 선언
 
     public GsFragment() {
         // Required empty public constructor
@@ -26,18 +41,31 @@ public class GsFragment extends Fragment {
         RecyclerView recyclerView = rootView.findViewById(R.id.recycler_view_gs);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
-        List<board_Item> goSchool = new ArrayList<>();
-        goSchool.add(new board_Item("등교 아이템 1", "차량번호 1", "010-0000-0000"));
-        goSchool.add(new board_Item("등교 아이템 2", "차량번호 2", "010-0000-0000"));
-        goSchool.add(new board_Item("등교 아이템 3", "차량번호 3", "010-0000-0000"));
-        goSchool.add(new board_Item("등교 아이템 4", "차량번호 4", "010-0000-0000"));
-        goSchool.add(new board_Item("등교 아이템 5", "차량번호 5", "010-0000-0000"));
-        goSchool.add(new board_Item("등교 아이템 6", "차량번호 6", "010-0000-0000"));
-        goSchool.add(new board_Item("등교 아이템 7", "차량번호 7", "010-0000-0000"));
-        goSchool.add(new board_Item("등교 아이템 8", "차량번호 8", "010-0000-0000"));
+        // ArrayList를 생성하여 Firestore에서 가져온 데이터를 저장
+        goSchool = new ArrayList<>();
 
-        gs_ListAdapter adapter1 = new gs_ListAdapter(getActivity(), goSchool);
+        // Firestore에서 데이터 읽어오기
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("User")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                User user = document.toObject(User.class);
+                                goSchool.add(user);
+                            }
+                            // 어댑터에게 데이터가 변경되었음을 알림
+                            adapter1.notifyDataSetChanged();
+                        } else {
+                            Toast.makeText(getActivity(), "데이터 로딩 실패", Toast.LENGTH_SHORT).show();
+                            Log.e(TAG, "데이터 로딩 실패", task.getException());
+                        }
+                    }
+                });
 
+        adapter1 = new gs_ListAdapter(getActivity(), goSchool); // adapter1을 초기화
         recyclerView.setAdapter(adapter1);
 
         SwipeCall swipeCall = new SwipeCall(requireContext(), adapter1);
--- app/src/main/java/com/yc/ac/gachigachi/gs_ListAdapter.java
+++ app/src/main/java/com/yc/ac/gachigachi/gs_ListAdapter.java
@@ -9,53 +9,64 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class gs_ListAdapter extends RecyclerView.Adapter<gs_ListAdapter.ViewHolder> {
-    private final List<board_Item> items;
+    //private final List<board_Item> items;
+    private ArrayList<User> arrayList;
+    private Context context;
 
-    public gs_ListAdapter(Context context, List<board_Item> items) {
-        this.items = items;
-    }
+    public gs_ListAdapter(Context context, ArrayList<User> items) {
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        public TextView titleTextView;
-        public TextView subTextView1;
-        public TextView PhoneNum;
-
-        public ViewHolder(View itemView) {
-            super(itemView);
-            titleTextView = itemView.findViewById(R.id.textViewTitle);
-            subTextView1 = itemView.findViewById(R.id.textViewSubtitle1);
-            PhoneNum = itemView.findViewById(R.id.textViewPhoneNum);
-        }
+        //this.items = items;
+        this.arrayList = arrayList;
+        this.context = context;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_board, parent, false);
-        return new ViewHolder(view);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
     }
 
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        board_Item item = items.get(position);
-        holder.titleTextView.setText(item.getTitle());
-        holder.subTextView1.setText(item.getSubText1());
-        holder.PhoneNum.setText(item.getPhoneNum());
-    }
-    public String getPhoneNumber(int position) {
-        if (position >= 0 && position < items.size()) {
-            return items.get(position).getPhoneNum();
-        } else {
-            return "";
-        }
+//        board_Item item = items.get(position);
+//        holder.titleTextView.setText(item.getTitle());
+//        holder.subTextView1.setText(item.getSubText1());
+//        holder.PhoneNum.setText(item.getPhoneNum());
+        holder.tv_name.setText(arrayList.get(position).getName());
+        holder.tv_carNumber.setText(arrayList.get(position).getCarNumber());
+        holder.tv_address.setText(arrayList.get(position).getAddress());
+        holder.tv_phoneNumber.setText(arrayList.get(position).getPhoneNumber());
     }
 
     @Override
     public int getItemCount() {
-        return items.size();
+        return (arrayList != null ? arrayList.size() : null);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+//        public TextView titleTextView;
+//        public TextView subTextView1;
+//        public TextView PhoneNum;
+        TextView tv_name;
+        TextView tv_carNumber;
+        TextView tv_address;
+        TextView tv_phoneNumber;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+//            titleTextView = itemView.findViewById(R.id.textViewTitle);
+//            subTextView1 = itemView.findViewById(R.id.textViewSubtitle1);
+//            PhoneNum = itemView.findViewById(R.id.textViewPhoneNum);
+            this.tv_name = itemView.findViewById(R.id.tv_name);
+            this.tv_carNumber = itemView.findViewById(R.id.tv_carNumber);
+            this.tv_address = itemView.findViewById(R.id.tv_address);
+            this.tv_phoneNumber = itemView.findViewById(R.id.tv_phoneNumber);
+        }
     }
 }
