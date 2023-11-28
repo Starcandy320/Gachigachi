922899e455ad2b761ebc5c3d499a1d35a62b1e80
ㅇㅇ <Starcandy320@outlook.com>
2023-11-28 10:24:55 +0900

    Merge pull request #3
    * 차량등록추가
    
    * 앱아이콘 수정(커스텀가능), 테마에 색상추가
    
    * 테마이름수정
    
    * 스플래시 화면을 통해 앱실행시 로딩화면 1초 보여주고 메인액티비티로 이동
    
    * 로고디자인변경
    
    * 액티비티에 겹치는 코드수정
    
    * 홈, 등교, 하교, 설정 프래그먼트 생성과 아이콘 등록
    
    * 등교리스트 구현 및 샘플데이터
    
    * 등교리스트 스와이프 구현
    
    * carAdd 부분 구현 시작
    
    * 상단 정보 입력 부분 구현
    
    * 버튼 setOnClickListener 설정
    
    * 커스텀 객체 CarList 작성
    
    * 시간표 처리 완성
    
    * 취소 버튼 구현
    
    * 취소 버튼 재구현
    
    * Merge branch 'master' into carAdd

--- app/src/main/java/com/yc/ac/gachigachi/CarReg.java
+++ app/src/main/java/com/yc/ac/gachigachi/CarReg.java
@@ -1,26 +1,162 @@
 package com.yc.ac.gachigachi;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class CarReg extends AppCompatActivity {
-    Button Monday;
+
+    // 일반 정보 부분
+    TextInputEditText editTextName;
+    TextInputEditText editTextCarNumber;
+    TextInputEditText editTextAddress;
+    TextInputEditText editTextPhoneNumber;
+
+    // 시간표 부분
+    TextInputEditText editTextMondayArrival;
+    TextInputEditText editTextTuesdayArrival;
+    TextInputEditText editTextWednesdayArrival;
+    TextInputEditText editTextThursdayArrival;
+    TextInputEditText editTextFridayArrival;
+    TextInputEditText editTextMondayDeparture;
+    TextInputEditText editTextTuesdayDeparture;
+    TextInputEditText editTextWednesdayDeparture;
+    TextInputEditText editTextThursdayDeparture;
+    TextInputEditText editTextFridayDeparture;
+
+    // 버튼 부분
+    MaterialButton submitButton;
+    MaterialButton cancelButton;
+
+    // TAG String 상수 선언
+    private static final String TAG= "firestore";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_reg);
-        Button Monday = findViewById(R.id.Monday);
 
-        Monday.setOnClickListener(new View.OnClickListener() {
+        // 일반 정보 부분
+        editTextName = findViewById(R.id.editTextName);
+        editTextCarNumber = findViewById(R.id.editTextCarNumber);
+        editTextAddress = findViewById(R.id.editTextAddress);
+        editTextPhoneNumber = findViewById(R.id.editTextPhoneNumber);
+        
+        // 시간표 부분
+        editTextMondayArrival = findViewById(R.id.editTextMondayArrival);
+        editTextTuesdayArrival = findViewById(R.id.editTextTuesdayArrival);
+        editTextWednesdayArrival = findViewById(R.id.editTextWednesdayArrival);
+        editTextThursdayArrival = findViewById(R.id.editTextThursdayArrival);
+        editTextFridayArrival = findViewById(R.id.editTextFridayArrival);
+        editTextMondayDeparture = findViewById(R.id.editTextMondayDeparture);
+        editTextTuesdayDeparture = findViewById(R.id.editTextTuesdayDeparture);
+        editTextWednesdayDeparture = findViewById(R.id.editTextWednesdayDeparture);
+        editTextThursdayDeparture = findViewById(R.id.editTextThursdayDeparture);
+        editTextFridayDeparture = findViewById(R.id.editTextFridayDeparture);
+
+        // button 부분
+        submitButton = findViewById(R.id.submitButton);
+        cancelButton = findViewById(R.id.cancelButton);
+
+
+
+        // Firestore 초기화
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        // cancelButton 클릭시 동작
+        cancelButton.setOnClickListener(v -> {
+            new MaterialAlertDialogBuilder(CarReg.this)
+                    .setTitle("경고")
+                    .setMessage("입력된 정보를 저장하지 않고 뒤로 갑니다.")
+                    .setNegativeButton("취소", (dialog, which) -> {dialog.dismiss(); })
+                    .setPositiveButton("확인", (dialog, which) -> { finish(); })
+                    .show();
+        });
+
+
+
+        // submitButton 클릭시 동작
+        submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), PopUp.class);
-                startActivity(intent);
+                // 시간표 후처리
+                Map<String, Object> mon = new HashMap<>();
+                mon.put("goSchool", editTextMondayArrival);
+                mon.put("goHome", editTextMondayDeparture);
+                mon.put("isShow_day", true);
+                Map<String, Object> tue = new HashMap<>();
+                tue.put("goSchool", editTextTuesdayArrival);
+                tue.put("goHome", editTextTuesdayDeparture);
+                tue.put("isShow_day", true);
+                Map<String, Object> wed = new HashMap<>();
+                wed.put("goSchool", editTextWednesdayArrival);
+                wed.put("goHome", editTextWednesdayDeparture);
+                wed.put("isShow_day", true);
+                Map<String, Object> thu = new HashMap<>();
+                thu.put("goSchool", editTextThursdayArrival);
+                thu.put("goHome", editTextThursdayDeparture);
+                thu.put("isShow_day", true);
+                Map<String, Object> fri = new HashMap<>();
+                fri.put("goSchool", editTextFridayArrival);
+                fri.put("goHome", editTextFridayDeparture);
+                fri.put("isShow_day", true);
+                List<Map<String, Object>> timetable = new ArrayList<>();
+                timetable.add(mon);
+                timetable.add(tue);
+                timetable.add(wed);
+                timetable.add(thu);
+                timetable.add(fri);
+
+                // 입력한 데이터를 받아와서 CarList 객체에 삽입
+                CarList carList = new CarList(
+                        editTextName.toString(),
+                        editTextCarNumber.toString(),
+                        editTextAddress.toString(),
+                        editTextPhoneNumber.toString(),
+                        true,
+                        timetable
+                );
+
+                // 자동 생성된 ID로 문서 생성
+                db.collection("car")
+                        .add(carList)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
+                                Toast.makeText(CarReg.this, "성공적으로 저장되었습니다", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error adding document", e);
+                                Toast.makeText(CarReg.this, "저장에 실패하였습니다", Toast.LENGTH_SHORT).show();
+                            }
+                        });
             }
         });
+
+
+
     }
 }
+
